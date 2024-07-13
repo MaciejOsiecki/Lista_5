de7f46d1701e7491b4f13cbe4dd476b35b4deed4
Maciej Osiecki <osiecki.maciej.v@gmail.com>
2024-07-13 13:26:46 +0200

    skrypt

--- zadanie.sh
+++ zadanie.sh
@@ -1,32 +1,58 @@
 #!/bin/bash
-# Skrypt do zadania
 
-# Flagi
-data=false
-logs=false
-help=false
+# Funkcja do wyświetlania imienia i nazwiska
+function wyswietl_imie_nazwisko() {
+    echo "Twoje Imię i Nazwisko"
+}
 
-# Parsowanie flag
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --date|-d) data=true ;;
-        --logs|-l) logs=true ;;
-        --help|-h) help=true ;;
-    esac
-    shift
-done
+# Funkcja do obliczania silni
+function oblicz_silnia() {
+    local n=$((52756 % 10))
+    local silnia=1
+    for (( i=1; i<=n; i++ )); do
+        silnia=$((silnia * i))
+    done
+    echo "Silnia z $n to: $silnia"
+}
 
-# Implementacja logiki flag
-if [ "$data" = true ]; then
-    echo "Wybrano flagę data."
-    # Dodaj tutaj swoją logikę
-fi
+# Funkcja Kwadrat
+function drukuj_kwadrat() {
+    local bok=$1
+    if [[ -z "$bok" ]]; then
+        read -p "Podaj długość boku: " bok
+    fi
+    for (( i=0; i<bok; i++ )); do
+        for (( j=0; j<bok; j++ )); do
+            echo -n "#"
+        done
+        echo
+    done
+}
 
-if [ "$logs" = true ]; then
-    echo "Wybrano flagę logs."
+# Funkcja do zapisywania słowa od tyłu w pliku dane.txt
+function zapisz_slowo_od_tylu() {
+    read -p "Podaj słowo: " slowo
+    od_tylu=$(echo $slowo | rev)
+    echo $od_tylu > dane.txt
+    echo "Słowo zapisane od tyłu w pliku dane.txt"
+}
 
-fi
-
-if [ "
-
+# Główna logika skryptu
+case "$1" in
+    -imie|--i)
+        wyswietl_imie_nazwisko
+        ;;
+    -silnia|--s)
+        oblicz_silnia
+        ;;
+    -kwadrat|--k)
+        drukuj_kwadrat $2
+        ;;
+    -zapis|--z)
+        zapisz_slowo_od_tylu
+        ;;
+    *)
+        ls -l ~
+        ;;
+esac
 
